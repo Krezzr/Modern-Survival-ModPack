b0743d19a79f0e77b70842174d942acaece5a400
Krezzr <102569995+Krezzr@users.noreply.github.com>
2022-04-13 23:38:33 -0400

    Update to 0.2
    Modern Survival 0.2 Changelogs
    *Updated Modern Survival Mod
    *Update REI Mod
    *Added Hydratation Mod
    *Added Environmentz Mod
    *Added Origins Mod
    
    *Added Waystone Mod
    *Remove RPG-HUD because it causes problem with Hydratation mod
    *Remove Armor point++ because it causes problem with Hydratation mod
    
    *Setup config for better integration of Hydratation and Environmentz

--- kubejs/client_scripts/script.js
+++
@@ -1,7 +0,0 @@
-// priority: 0
-
-console.info('Hello, World! (You will see this line every time client resources reload)')
-
-onEvent('rei.hide.items', event => {
-	
-})
--- kubejs/server_scripts/settings.js
+++ kubejs/server_scripts/settings.js
@@ -1,4 +1,11 @@
+ /*
 settings.logAddedRecipes = true
 settings.logRemovedRecipes = true
 settings.logSkippedRecipes = false
 settings.logErroringRecipes = true
+ */ // /*
+settings.logAddedRecipes = false
+settings.logRemovedRecipes = false
+settings.logSkippedRecipes = false
+settings.logErroringRecipes = false
+// */
